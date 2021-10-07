9b74156a08eb49d71182271997a972bfa1398170
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-07 08:46:56 +0300

    update highest_scores_report_in_exam_top_5.sql with sql script

---
+++ highest_scores_report_in_exam_top_5.sql
@@ -0,0 +1,26 @@
+USE  online_exam;
+
+SELECT 
+pupils.first_name,
+pupils.last_name,
+answers.pupil_id,
+count(choices.remark) AS correctly_asnswered,
+(count(choices.remark)*questions.marks) AS total_marks,
+(((count(choices.remark)*questions.marks)/exams.total_marks)*100) AS percentage_score,
+
+exams.exam_title
+
+
+FROM answers
+RIGHT JOIN choices ON answers.choice_id = choices.choice_id
+RIGHT JOIN questions ON answers.question_id = questions.question_id
+RIGHT JOIN pupils ON answers.pupil_id = pupils.pupil_id
+LEFT JOIN exams ON questions.exam_id = exams.exam_id
+
+
+WHERE remark= 'Correct'
+
+GROUP BY pupil_id
+
+ORDER BY percentage DESC
+LIMIT 5;
