ccfa993a0cab4cf84bd03454b9c4d63ef3afe860
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-13 14:54:41 +0300

    add & update question_choices_answer_generator_exam3.sql with exam 3 choices

--- question_choices_answer_generator_exam3.sql
+++ question_choices_answer_generator_exam3.sql
@@ -14,3 +14,22 @@ BEGIN
 END$$
 DELIMITER ;
 CALL generate_questions_exam3();
+
+-- exam 3 choices***
+DROP PROCEDURE IF EXISTS generate_choices_exam3;
+DELIMITER $$
+CREATE PROCEDURE generate_choices_exam3()
+BEGIN
+  DECLARE i INT DEFAULT 51;
+  WHILE i < 76 DO
+    INSERT INTO `choices` (`question_id`,`choice_label`,`remark`) 
+		VALUES 
+        (i,'A','Wrong'),
+        (i,'B','Wrong'),
+        (i,'C','Correct'),
+        (i,'D','Wrong');
+    SET i = i + 1;
+  END WHILE;
+END$$
+DELIMITER ;
+CALL generate_choices_exam3();
