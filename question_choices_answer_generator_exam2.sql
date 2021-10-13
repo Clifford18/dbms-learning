262ee9433051be50e2df4afe9318840062f0b135
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-13 13:36:15 +0300

    add and update question_choices_answer_generator_exam2.sql file with exam 2 choices scripts

--- question_choices_answer_generator_exam2.sql
+++ question_choices_answer_generator_exam2.sql
@@ -1,5 +1,4 @@
 -- exam 2 questions***
-
 DROP PROCEDURE IF EXISTS generate_questions_exam2;
 DELIMITER $$
 CREATE PROCEDURE generate_questions_exam2()
@@ -16,3 +15,22 @@ END$$
 DELIMITER ;
 CALL generate_questions_exam2();
 
+
+-- exam 2 choices***
+DROP PROCEDURE IF EXISTS generate_choices_exam2;
+DELIMITER $$
+CREATE PROCEDURE generate_choices_exam2()
+BEGIN
+  DECLARE i INT DEFAULT 26;
+  WHILE i < 51 DO
+    INSERT INTO `choices` (`question_id`,`choice_label`,`remark`) 
+		VALUES 
+        (i,'A','Wrong'),
+        (i,'B','Wrong'),
+        (i,'C','Correct'),
+        (i,'D','Wrong');
+    SET i = i + 1;
+  END WHILE;
+END$$
+DELIMITER ;
+CALL generate_choices_exam2();
