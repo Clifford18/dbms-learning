ced896e206d14db40f5261a58f1063a8c0a95855
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-15 11:08:29 +0300

    update employee_salaries.sql with tables

---
+++ employee_salaries.sql
@@ -0,0 +1,33 @@
+-- status
+-- genders
+-- status
+
+-- department
+-- employees
+-- managers_description
+-- managers
+
+-- performance_reviews
+
+-- training_history
+-- courses
+
+-- skills
+-- skill_levels
+-- employee_skills
+
+-- relationships
+-- relationship_descriptions
+-- dependants
+
+-- designations
+-- employee_designations
+
+-- projects
+-- tasks
+
+
+
+
+
+
