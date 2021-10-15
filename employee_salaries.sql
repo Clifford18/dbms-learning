0bea0821e37ccce293442db42c68bdb5e65a19d5
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-15 16:10:02 +0300

    employee_salaries.sql file on enums

--- employee_salaries.sql
+++ employee_salaries.sql
@@ -36,7 +36,7 @@ USE `employee_salaries`;
 DROP TABLE IF EXISTS `status`;
 
 CREATE TABLE `status` (
-`status`        	varchar(30) 	NOT NULL,
+`status`        	ENUM	('new','active','leaving','terminated'),
 `date_created`  	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `date_modified` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
@@ -72,6 +72,46 @@ PRIMARY KEY (`department_name`),
 
 KEY `index_subjects_date_created`(`date_created`),
 KEY `index_subjects_date_modified`(`date_modified`)
+);
+
+
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `employees` (
+`employee_id`	          bigint	 		NOT NULL AUTO_INCREMENT,
+`first_name` 	          varchar(30) 	NOT NULL,
+`last_name` 	          varchar(30) 	NOT NULL,
+`gender`                varchar(30) 	NOT NULL,
+`id_number` 	          varchar(30) 	NOT NULL,
+`phone_number` 	        varchar(30) 	NOT NULL,
+`email_address` 	      varchar(30) 	NOT NULL,
+`date_of_birth` 	      varchar(30) 	NOT NULL,
+`department_name` 	    varchar(30) 	NOT NULL,
+`initial_basic_pay` 	  varchar(30) 	NOT NULL,
+`status` 	              ENUM	('new','active','leaving','terminated'),
+`employment_start_date` 	varchar(30) 	NOT NULL,
+`employment_end_date` 	varchar(30) 	NOT NULL,
+
+`date_created` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`date_modified` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+PRIMARY KEY (`employee_id`),
+
+KEY `index_employees_phone_number`(`phone_number`),
+KEY `index_employees_email_address`(`email_address`),
+KEY `index_employees_date_created`(`date_created`),
+KEY `index_employees_date_modified`(`date_modified`),
+
+UNIQUE KEY `uindex_employees_id_number` (`id_number`),
+
+KEY `fk_genders_employees_gender` (`gender`),
+CONSTRAINT `fk_genders_employees_gender` FOREIGN KEY (`gender`) REFERENCES `genders` (`gender`) ON DELETE RESTRICT ON UPDATE CASCADE,
+
+KEY `fk_departments_employees_gender` (`department_name`),
+CONSTRAINT `fk_departments_employees_gender` FOREIGN KEY (`department_name`) REFERENCES `departments` (`department_name`) ON DELETE RESTRICT ON UPDATE CASCADE,
+
+KEY `fk_status_employees_status` (`status`),
+CONSTRAINT `fk_status_employees_status` FOREIGN KEY (`status`) REFERENCES `status` (`status`) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
