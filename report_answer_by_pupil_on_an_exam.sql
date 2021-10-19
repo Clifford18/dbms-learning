e8a52ea14856e8d2ec871f16e3723db9db79538b
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-19 11:42:09 +0300

    update report_answer_by_pupil_on_an_exam.sql file

--- report_answer_by_pupil_on_an_exam.sql
+++ report_answer_by_pupil_on_an_exam.sql
@@ -3,15 +3,17 @@ USE  online_exam;
 SELECT 
 full_name(p.first_name,p.last_name) AS fullnames,
 a.pupil_id,
-c.remark,
 q.question_id,
+q.marks AS question_marks,
+c.remark,
+
 e.exam_title
 
-FROM answers AS a
-LEFT JOIN choices AS c ON a.choice_id = c.choice_id
-LEFT JOIN questions AS q ON a.question_id = q.question_id
-LEFT JOIN pupils AS p ON a.pupil_id = p.pupil_id
-LEFT JOIN exams AS e ON q.exam_id = e.exam_id
+FROM answers a
+RIGHT JOIN choices c ON a.choice_id = c.choice_id
+RIGHT JOIN questions q ON a.question_id = q.question_id
+RIGHT JOIN pupils p ON a.pupil_id = p.pupil_id
+LEFT JOIN exams e ON q.exam_id = e.exam_id
 
 WHERE p.pupil_id= 1 AND e.exam_id = 2
 ;
