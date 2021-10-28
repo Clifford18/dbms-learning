87a1db5969a1340e2f968ca898c795e502889bcb
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-28 14:19:25 +0300

    update score_report_for_all_pupils_ranked_from_highest.sql file

---
+++ score_report_for_all_pupils_ranked_from_highest.sql
@@ -0,0 +1,33 @@
+USE  online_exam;
+
+SET @TotalMarks = (SELECT 
+SUM(q.marks) 
+FROM questions AS q
+LEFT JOIN exams AS e ON q.exam_id = e.exam_id
+WHERE e.exam_id = 1
+);
+
+SELECT 
+full_name(p.first_name,p.last_name) AS fullnames,
+a.pupil_id,
+count(q.question_id) AS correctly_answered,
+SUM(q.marks) AS question_marks,
+ROUND(((SUM(q.marks)/@TotalMarks)*100),1) AS Percentage_Score ,
+
+e.exam_title
+
+FROM answers a
+RIGHT JOIN choices c ON a.choice_id = c.choice_id
+RIGHT JOIN questions q ON a.question_id = q.question_id
+RIGHT JOIN pupils p ON a.pupil_id = p.pupil_id
+LEFT JOIN exams e ON q.exam_id = e.exam_id
+
+WHERE e.exam_id = 1 and remark='Correct'
+
+GROUP BY p.pupil_id
+
+HAVING Percentage_Score=Percentage_Score
+
+ORDER BY Percentage_Score DESC
+
+;
