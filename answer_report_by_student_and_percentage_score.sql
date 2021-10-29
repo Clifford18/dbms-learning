1c6885c1fb657219c104b85a9453fecde30108b1
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-29 17:00:34 +0300

    update answer_report_by_student_and_percentage_score.sql file

--- answer_report_by_student_and_percentage_score.sql
+++ answer_report_by_student_and_percentage_score.sql
@@ -1,23 +1,29 @@
 USE  online_exam;
 
+SET @TotalMarks = (SELECT
+SUM(q.marks)
+FROM questions AS q
+LEFT JOIN exams AS e ON q.exam_id = e.exam_id
+WHERE e.exam_id = 1
+);
+
 SELECT 
-pupils.first_name,
-pupils.last_name,
-answers.pupil_id,
-count(choices.remark) AS correctly_asnswered,
-(count(choices.remark)*questions.marks) AS total_marks,
-ROUND((((count(choices.remark)*questions.marks)/exams.total_marks)*100),1) AS percentage_score,
+p.first_name,
+p.last_name,
+a.pupil_id,
+count(c.remark) AS correctly_asnswered,
+(count(c.remark)*q.marks) AS total_marks,
+ROUND(((SUM(q.marks)/@TotalMarks)*100),1) AS Percentage_Score ,
+e.exam_title
 
-exams.exam_title
+FROM answers a
+RIGHT JOIN choices c ON a.choice_id = c.choice_id
+RIGHT JOIN questions q ON a.question_id = q.question_id
+RIGHT JOIN pupils p ON a.pupil_id = p.pupil_id
+LEFT JOIN exams e ON q.exam_id = e.exam_id
 
-FROM answers
-RIGHT JOIN choices ON answers.choice_id = choices.choice_id
-RIGHT JOIN questions ON answers.question_id = questions.question_id
-RIGHT JOIN pupils ON answers.pupil_id = pupils.pupil_id
-LEFT JOIN exams ON questions.exam_id = exams.exam_id
+WHERE c.remark= 'Correct'
 
-WHERE remark= 'Correct'
+GROUP BY p.pupil_id
 
-GROUP BY pupil_id
-
-ORDER BY pupil_id;
+ORDER BY p.pupil_id;
